2afd745dd9cd2aaf10e06f0617220b9947770a71
drstranges <drstranges@gmail.com>
2015-12-23 22:42:17 +0200

    Add Settings Screen

--- app/src/main/java/com/testapp/weather/view/MainActivity.java
+++ app/src/main/java/com/testapp/weather/view/MainActivity.java
@@ -2,7 +2,7 @@ package com.testapp.weather.view;
 
 import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
-import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -10,11 +10,9 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.testapp.weather.BuildConfig;
 import com.testapp.weather.R;
 import com.testapp.weather.databinding.ActivityMainBinding;
 import com.testapp.weather.util.KeyboardUtils;
--- app/src/main/java/com/testapp/weather/view/fragment/SettingsFragment.java
+++ app/src/main/java/com/testapp/weather/view/fragment/SettingsFragment.java
@@ -1,9 +1,17 @@
 package com.testapp.weather.view.fragment;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.testapp.weather.R;
 
 /**
  * Created on 23.12.2015.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle _bundle, String _s) {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
 }
