133f088f7bd2e14cdd5847fa40002945d6dcad26
drstranges <drstranges@gmail.com>
2015-12-25 01:34:59 +0200

    Show preferences summary

--- app/src/main/java/com/testapp/weather/view/fragment/SettingsFragment.java
+++ app/src/main/java/com/testapp/weather/view/fragment/SettingsFragment.java
@@ -1,6 +1,9 @@
 package com.testapp.weather.view.fragment;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.testapp.weather.R;
@@ -8,10 +11,36 @@ import com.testapp.weather.R;
 /**
  * Created on 23.12.2015.
  */
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle _bundle, String _s) {
         addPreferencesFromResource(R.xml.pref_general);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+        return true;
     }
 }
